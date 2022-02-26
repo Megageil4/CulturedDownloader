583db9956a0daed82bd9e0123b3f2b8b43daeb3e
Megageil4 <78154975+Megageil4@users.noreply.github.com>
2022-02-26 22:53:25 +0100

    Add files via upload

---
+++ Tags.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CulturedDownloader
+{
+    public partial class Tags : Form
+    {
+        public Tags()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (var tag in checkedListBox1.CheckedItems)
+            {
+                MessageBox.Show(tag.ToString());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
