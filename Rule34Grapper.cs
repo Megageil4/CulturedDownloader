10b9eabcd00c50bb9660cce467d17cd8c19315ca
Megageil4 <78154975+Megageil4@users.noreply.github.com>
2022-04-04 19:50:59 +0200

    Add files via upload

---
+++ Rule34Grapper.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CulturedDownloaderV3
+{
+    class Rule34Grapper
+    {
+
+        public static List<string> GetChildPages(string parentUrl, string xPath, string childSubstring,HtmlAgilityPack.HtmlWeb webloader)
+        {
+            HtmlAgilityPack.HtmlDocument parentpage = webloader.Load(parentUrl);
+            List<string> childUrls = new List<string>();
+
+            foreach (var imagePreview in parentpage.DocumentNode.SelectNodes(xPath))
+            {
+                string imageSource = imagePreview.InnerHtml;
+                imageSource = childSubstring + getBetween(imageSource,"id=\"","\"").Substring(1);
+                childUrls.Add(imageSource);
+            }
+
+            return childUrls;
+        }
+
+        public static List<string> GetImages(List<string> childUrls,string imageXpath, string videoXpath, HtmlAgilityPack.HtmlWeb webloader)
+        {
+            List<string> images = new List<string>();
+
+            for (int i = 0; i < childUrls.Count; i++)
+            {
+                HtmlAgilityPack.HtmlDocument website = webloader.Load(childUrls[i]);
+                string imageUrl = "";
+                try
+                {
+                    imageUrl = website.DocumentNode.SelectNodes(imageXpath)[0].OuterHtml;
+                }
+                catch
+                {
+                    imageUrl = website.DocumentNode.SelectSingleNode(videoXpath).OuterHtml;
+                }
+                imageUrl = getBetween(imageUrl, "src=\"", "\"");
+                images.Add(imageUrl);
+
+            }
+
+            return images;
+        }
+
+        public static void OpenImagesInBrowser(List<string> images, bool incognito)
+        {
+            for (int i = 0; i < images.Count; i++)
+            {
+                using (var process = new Process())
+                {
+                    process.StartInfo.FileName = @"C:\Program Files\Google\Chrome\Application\chrome.exe";
+                    if (incognito)
+                    {
+                        process.StartInfo.Arguments = images[i] + " --incognito";
+                    }
+                    process.Start();
+                }
+            }
+
+        }
+
+        private static string getBetween(string strSource, string strStart, string strEnd)
+        {
+            if (strSource.Contains(strStart) && strSource.Contains(strEnd))
+            {
+                int Start, End;
+                Start = strSource.IndexOf(strStart, 0) + strStart.Length;
+                End = strSource.IndexOf(strEnd, Start);
+                return strSource.Substring(Start, End - Start);
+            }
+
+            return "";
+        }
+    }
+}
---
+++ TagSelection.Designer.cs
@@ -0,0 +1,267 @@
+﻿
+namespace CulturedDownloaderV3
+{
+    partial class TagSelection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.AddButton = new FontAwesome.Sharp.IconButton();
+            this.iconButton1 = new FontAwesome.Sharp.IconButton();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.AllTagsList = new System.Windows.Forms.ListBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.SelectedTagsList = new System.Windows.Forms.ListBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.iconButton3 = new FontAwesome.Sharp.IconButton();
+            this.iconButton2 = new FontAwesome.Sharp.IconButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SearchBar = new System.Windows.Forms.TextBox();
+            this.panel3.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.panel6);
+            this.panel3.Controls.Add(this.panel5);
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Controls.Add(this.panel2);
+            this.panel3.Controls.Add(this.panel1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(800, 450);
+            this.panel3.TabIndex = 3;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.AddButton);
+            this.panel6.Controls.Add(this.iconButton1);
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel6.Location = new System.Drawing.Point(370, 72);
+            this.panel6.Name = "panel6";
+            this.panel6.Padding = new System.Windows.Forms.Padding(0, 60, 0, 60);
+            this.panel6.Size = new System.Drawing.Size(60, 288);
+            this.panel6.TabIndex = 4;
+            // 
+            // AddButton
+            // 
+            this.AddButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.AddButton.FlatAppearance.BorderSize = 0;
+            this.AddButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AddButton.IconChar = FontAwesome.Sharp.IconChar.ChevronRight;
+            this.AddButton.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.AddButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.AddButton.Location = new System.Drawing.Point(0, 60);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(60, 60);
+            this.AddButton.TabIndex = 1;
+            this.AddButton.UseVisualStyleBackColor = true;
+            // 
+            // iconButton1
+            // 
+            this.iconButton1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.iconButton1.FlatAppearance.BorderSize = 0;
+            this.iconButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton1.IconChar = FontAwesome.Sharp.IconChar.ChevronLeft;
+            this.iconButton1.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton1.Location = new System.Drawing.Point(0, 168);
+            this.iconButton1.Name = "iconButton1";
+            this.iconButton1.Size = new System.Drawing.Size(60, 60);
+            this.iconButton1.TabIndex = 0;
+            this.iconButton1.UseVisualStyleBackColor = true;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.AllTagsList);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel5.Location = new System.Drawing.Point(0, 72);
+            this.panel5.Margin = new System.Windows.Forms.Padding(10);
+            this.panel5.Name = "panel5";
+            this.panel5.Padding = new System.Windows.Forms.Padding(15, 10, 10, 10);
+            this.panel5.Size = new System.Drawing.Size(370, 288);
+            this.panel5.TabIndex = 1;
+            // 
+            // AllTagsList
+            // 
+            this.AllTagsList.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.AllTagsList.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.AllTagsList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AllTagsList.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.AllTagsList.FormattingEnabled = true;
+            this.AllTagsList.ItemHeight = 15;
+            this.AllTagsList.Location = new System.Drawing.Point(15, 10);
+            this.AllTagsList.Name = "AllTagsList";
+            this.AllTagsList.Size = new System.Drawing.Size(345, 268);
+            this.AllTagsList.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.SelectedTagsList);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel4.Location = new System.Drawing.Point(430, 72);
+            this.panel4.Name = "panel4";
+            this.panel4.Padding = new System.Windows.Forms.Padding(10, 10, 15, 10);
+            this.panel4.Size = new System.Drawing.Size(370, 288);
+            this.panel4.TabIndex = 0;
+            // 
+            // SelectedTagsList
+            // 
+            this.SelectedTagsList.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.SelectedTagsList.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.SelectedTagsList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SelectedTagsList.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.SelectedTagsList.FormattingEnabled = true;
+            this.SelectedTagsList.ItemHeight = 15;
+            this.SelectedTagsList.Location = new System.Drawing.Point(10, 10);
+            this.SelectedTagsList.Name = "SelectedTagsList";
+            this.SelectedTagsList.Size = new System.Drawing.Size(345, 268);
+            this.SelectedTagsList.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.iconButton3);
+            this.panel2.Controls.Add(this.iconButton2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 360);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(60, 15, 60, 15);
+            this.panel2.Size = new System.Drawing.Size(800, 90);
+            this.panel2.TabIndex = 3;
+            // 
+            // iconButton3
+            // 
+            this.iconButton3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(65)))), ((int)(((byte)(68)))));
+            this.iconButton3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.iconButton3.FlatAppearance.BorderSize = 0;
+            this.iconButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton3.Font = new System.Drawing.Font("Segoe UI", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.iconButton3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.iconButton3.IconChar = FontAwesome.Sharp.IconChar.Trash;
+            this.iconButton3.IconColor = System.Drawing.Color.Firebrick;
+            this.iconButton3.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton3.IconSize = 57;
+            this.iconButton3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButton3.Location = new System.Drawing.Point(490, 15);
+            this.iconButton3.Name = "iconButton3";
+            this.iconButton3.Padding = new System.Windows.Forms.Padding(20, 0, 23, 0);
+            this.iconButton3.Size = new System.Drawing.Size(250, 60);
+            this.iconButton3.TabIndex = 1;
+            this.iconButton3.Text = "Cancel";
+            this.iconButton3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.iconButton3.UseVisualStyleBackColor = false;
+            // 
+            // iconButton2
+            // 
+            this.iconButton2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(65)))), ((int)(((byte)(68)))));
+            this.iconButton2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.iconButton2.FlatAppearance.BorderSize = 0;
+            this.iconButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton2.Font = new System.Drawing.Font("Segoe UI", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.iconButton2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.iconButton2.IconChar = FontAwesome.Sharp.IconChar.Check;
+            this.iconButton2.IconColor = System.Drawing.Color.SeaGreen;
+            this.iconButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton2.IconSize = 70;
+            this.iconButton2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButton2.Location = new System.Drawing.Point(60, 15);
+            this.iconButton2.Name = "iconButton2";
+            this.iconButton2.Padding = new System.Windows.Forms.Padding(20, 0, 23, 0);
+            this.iconButton2.Size = new System.Drawing.Size(250, 60);
+            this.iconButton2.TabIndex = 0;
+            this.iconButton2.Text = "Confirm";
+            this.iconButton2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.iconButton2.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.SearchBar);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(15, 13, 15, 10);
+            this.panel1.Size = new System.Drawing.Size(800, 72);
+            this.panel1.TabIndex = 2;
+            // 
+            // SearchBar
+            // 
+            this.SearchBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(65)))), ((int)(((byte)(68)))));
+            this.SearchBar.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.SearchBar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SearchBar.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SearchBar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.SearchBar.Location = new System.Drawing.Point(15, 13);
+            this.SearchBar.Multiline = true;
+            this.SearchBar.Name = "SearchBar";
+            this.SearchBar.Size = new System.Drawing.Size(770, 49);
+            this.SearchBar.TabIndex = 0;
+            // 
+            // TagSelection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "TagSelection";
+            this.Text = "TagSelection";
+            this.panel3.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.ListBox AllTagsList;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.ListBox SelectedTagsList;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel6;
+        private FontAwesome.Sharp.IconButton AddButton;
+        private FontAwesome.Sharp.IconButton iconButton1;
+        private System.Windows.Forms.TextBox SearchBar;
+        private FontAwesome.Sharp.IconButton iconButton3;
+        private FontAwesome.Sharp.IconButton iconButton2;
+    }
+}
